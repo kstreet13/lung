545b688fe2441603f84fbf770eca2cda09207f31
Kelly Street <street.kelly@gmail.com>
2026-03-02 15:53:10 -0800

    integrate with metadata, start cellchat

---
+++ cellchat.R
@@ -0,0 +1,7 @@
+# CellChat
+# communication between fibroblasts and epithelial cells
+# split by stages of development
+# mesenchym ~= myofibroblast
+# myofibroblast communication with AT1/AT2
+# could compare our P14 to their P14, their P3/P7/P14
+
--- integrated.R
+++ integrated.R
@@ -50,24 +50,63 @@ common.genes <- rownames(hx)[rownames(hx) %in% rownames(ct)]
 
 sce <- batchCorrect(hx[common.genes, ], ct[common.genes],
                     assay.type = 'resids',
-                    PARAM = FastMnnParam())
+                    PARAM = ClassicMnnParam())
 sce$batch <- factor(sce$batch)
+levels(sce$batch) <- c('hx','ct')
 
-reducedDim(sce,'umap') <- uwot::umap(reducedDim(sce,'corrected')[,1:30])
+reducedDim(sce,'pca') <- BiocSingular::runPCA(t(assay(sce,'corrected')), rank = 30)$x
+reducedDim(sce,'umap') <- uwot::umap(reducedDim(sce,'pca')[,1:30])
+
+saveRDS(sce, file = 'data/integrated.rds')
+
 
 shuf <- sample(ncol(sce))
 
 plot(reducedDim(sce,'umap')[shuf,], asp=1, col=colorby(sce$batch[shuf], alpha=.5), cex=.5)
 
 layout(matrix(1:2, nrow=1))
-ind <- which(sce$batch==1)
+ind <- which(sce$batch=='hx')
 plot(reducedDim(sce,'umap'), asp=1, col='grey90', cex=.5, main='Hypoxia')
 points(reducedDim(sce,'umap')[ind,], col=colorby(sce$batch, alpha=.5)[ind], cex=.5)
-ind <- which(sce$batch==2)
+ind <- which(sce$batch=='ct')
 plot(reducedDim(sce,'umap'), asp=1, col='grey90', cex=.5, main='Control (Ours)')
 points(reducedDim(sce,'umap')[ind,], col=colorby(sce$batch, alpha=.5)[ind], cex=.5)
 
 
+# add metadata
+sce <- readRDS('data/integrated.rds')
+colData(sce)$Cell_ID <- colnames(sce)
+# load existing metadata
+meta <- read.csv('~/OneDrive - University of Southern California/lung_data/GSE151974_cell_metadata_postfilter.csv.gz')
+names(meta)[1] <- 'Cell_ID'
+meta$percent.mt <- NULL # redundant with percent.mito
+# add easy stuff from our data
+ct <- Read10X('~/OneDrive - University of Southern California/lung_data/PN14_Ctrl_filtered_feature_bc_matrix/')
+ct <- CreateSeuratObject(counts = ct)
+ct$Cell_ID <- colnames(ct)
+ct$Age <- 'P14'
+ct$Oxygen <- 'Normoxia'
+ct$Sample <- 'P14_Normoxia_Ours'
+meta <- merge(meta, ct@meta.data, all.x = TRUE, all.y = TRUE, sort = FALSE)
+
+# check before merging
+all(meta$Cell_ID %in% colnames(sce))
+all(colnames(sce) %in% meta$Cell_ID)
+# merge (preserving colnames(sce) and rownames(colData(sce)))
+cd <- merge(colData(sce), meta, by = 'Cell_ID', all.x = TRUE, sort = FALSE)
+rownames(cd) <- cd$Cell_ID
+colData(sce) <- cd
+rm(ct,cd,meta)
+# convert to factors
+sce$orig.ident <- factor(sce$orig.ident)
+sce$Age <- factor(sce$Age)
+sce$Oxygen <- factor(sce$Oxygen)
+sce$Phase <- factor(sce$Phase)
+sce$Sample <- factor(sce$Sample)
+sce$cluster_high_res <- factor(sce$cluster_high_res)
+sce$CellType <- factor(sce$CellType)
+
+saveRDS(sce, file = 'data/integrated.rds')
 
 
 
